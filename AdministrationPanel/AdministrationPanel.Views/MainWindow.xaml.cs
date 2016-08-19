f8c96459476ef87c87eec787c6e46bd6f3ba93c8
Tomasz Klimek <tklimek@avaya.com>
2016-08-19 23:22:55 +0200

    Remove user

--- AdministrationPanel/AdministrationPanel.Views/MainWindow.xaml.cs
+++ AdministrationPanel/AdministrationPanel.Views/MainWindow.xaml.cs
@@ -77,6 +77,7 @@ namespace AdministrationPanel.Views
                 }
             }, 1);
 
+			IEnumerable<User> Users;
 			model.GetUsers((users, error) =>
             {
 				if (error != null)
@@ -86,6 +87,21 @@ namespace AdministrationPanel.Views
 				else
 				{
 					MessageBox.Show(String.Join(", ", users));
+					Users = users;
+
+					var UserItem = users.ElementAt(100);
+
+					model.RemoveUser((error2) =>
+					{
+						if (error != null)
+						{
+							MessageBox.Show(error2);
+						}
+						else
+						{
+							MessageBox.Show("User removed");
+						}
+					}, UserItem._id);
 				}
             });
             
