3dce201eafa0a876cae9ed95592ad61b8c6db656
Adam Kozlowski <akozlowski@avaya.com>
2016-08-19 20:41:04 +0200

    RestSharp

---
+++ AdministrationPanel/AdministrationPanel.Model/AdministrationPanel.Model.h
@@ -0,0 +1,13 @@
+// AdministrationPanel.Model.h
+
+#pragma once
+
+using namespace System;
+
+namespace AdministrationPanelModel {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
