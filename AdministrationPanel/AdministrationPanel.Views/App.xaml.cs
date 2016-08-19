70b5624fe6483783ea38ab90d397616bd4f3f57a
Adrian Szuszkiewicz <adrian.szuszkiewicz@grapeup.com>
2016-08-19 20:31:02 +0200

    Integrated Autofac

--- AdministrationPanel/AdministrationPanel.Views/App.xaml.cs
+++ AdministrationPanel/AdministrationPanel.Views/App.xaml.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AdministrationPanel.Views
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Bootstrap();
+            var window = bootstrapper.GetMainWindow();
+            window.Show();
+        }
     }
 }
