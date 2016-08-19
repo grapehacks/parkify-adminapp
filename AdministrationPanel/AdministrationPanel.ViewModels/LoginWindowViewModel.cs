62c226d8588ce09a63b6ab29bdda94d808ed5b5a
Adrian Szuszkiewicz <adrian.szuszkiewicz@grapeup.com>
2016-08-20 01:51:32 +0200

    Login window pt1

---
+++ AdministrationPanel/AdministrationPanel.ViewModels/LoginWindowViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using GalaSoft.MvvmLight.CommandWpf;
+using Model;
+using Model.DataTypes;
+
+namespace AdministrationPanel.ViewModels
+{
+    public class LoginWindowViewModel : AdministrationPanelViewModelBase
+    {
+        private readonly IDataProvider _dataProvider;
+        private readonly RelayCommand _loginCommand;
+        private string _email;
+        private string _error;
+        private string _password;
+
+        public LoginWindowViewModel(IDataProvider dataProvider)
+        {
+            _dataProvider = dataProvider;
+            _loginCommand = new RelayCommand(Login);
+        }
+
+        public string Error
+        {
+            get { return _error; }
+            set
+            {
+                if (value == _error) return;
+                _error = value;
+                OnPropertyChanged(() => Error);
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (value == _email) return;
+                _email = value;
+                OnPropertyChanged(() => Email);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (value == _password) return;
+                _password = value;
+                OnPropertyChanged(() => Password);
+            }
+        }
+
+        private async void Login()
+        {
+            var credential = new Credentials
+            {
+                Email = Email,
+                Password = Password
+            };
+
+            var result = await _dataProvider.Authenticate(credential);
+
+            if (result)
+            {
+                MessageBox.Show("Success");
+            }
+            else
+            {
+                Error = "Login failed";
+            }
+        }
+    }
+}
