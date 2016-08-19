10c74e9dd8f646510caa1331d7b4dc8cc15245ac
Adam Kozlowski <akozlowski@avaya.com>
2016-08-19 20:52:14 +0200

    Modesl

---
+++ AdministrationPanel/Model/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Card
+    {
+        public string Name;
+        public string Type;
+        public bool Removed;
+        public bool Active;
+        public User UserValue; // ?
+    }
+}
---
+++ AdministrationPanel/Model/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public enum UserType
+    {
+        User,
+        Admin
+    }
+
+    public enum UserParticipate
+    {
+        Yes,
+        No
+    }
+
+    public class User
+    {
+        public string Email;
+        public string Password;
+        public string Name;
+        public UserType Type;
+        public Card CardValue; //? 
+        public bool RememberLastChoice;
+        public bool Removed;
+        public int UnreadMsgCounter;
+    }   
+}
