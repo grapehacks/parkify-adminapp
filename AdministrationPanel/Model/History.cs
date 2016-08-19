494b79ea9fe0943c267948b039798656a0744b86
Adam Kozlowski <akozlowski@avaya.com>
2016-08-19 20:56:18 +0200

    Moar model

---
+++ AdministrationPanel/Model/History.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class History
+    {
+        public DateTime Date;
+        public Array<Draw> Winners;
+        public IEnumerable<User> Users;
+        public IEnumerable<String> SpecialCases;
+    }
+}
