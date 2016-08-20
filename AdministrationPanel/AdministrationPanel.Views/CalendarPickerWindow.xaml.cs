aed22e87c38bc3d4f37008568f2abc4169baba41
Adam Kozlowski <akozlowski@avaya.com>
2016-08-20 05:32:27 +0200

    Missing files

---
+++ AdministrationPanel/AdministrationPanel.Views/CalendarPickerWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdministrationPanel.Views
+{
+    /// <summary>
+    /// Interaction logic for CalendarPickerWindow.xaml
+    /// </summary>
+    public partial class CalendarPickerWindow : Window
+    {
+        public DateTime Date
+        {
+            get;
+            private set;
+        }
+
+        public CalendarPickerWindow()
+        {
+            InitializeComponent();
+            this.datePicker.DisplayDateStart = DateTime.Today;
+        }
+
+
+        private void Cancel(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
